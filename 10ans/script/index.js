5358417e537e4a36d0add6a5a2ab3fb2f24f3343
Philippe Lam <philippe.lam.ny@gmail.com>
2023-11-05 13:12:30 +0100

    ajout title

--- 10ans/script/index.js
+++ 10ans/script/index.js
@@ -5,33 +5,47 @@ function getevents() {
         })
         .then(function(data) {
             const currentEvent = data.events[0]
-            //displayTitle(currentEvent.title)
+            displayTitle(currentEvent.title)
             displayTimeline(currentEvent.timeline)
 
         })
-
 }
 
 function displayTitle(title) {
-    const containerTitle = document.getElementById('title')
+    const containerTitle = document.getElementById('main-event')
     containerTitle.innerHTML = title
 }
 
 function displayTimeline(events) {
     const containerEvents = document.getElementById('events')
     const dateNow = new Date()
-    const hoursnow = dateNow.getUTCHours() + ""
-    const minutesnow = dateNow.getUTCMinutes() + ""
-    const timeNow = hoursnow.padStart(2, '0') + ':' + minutesnow.padStart(2, '0');
+    const hourNow = dateNow.getUTCHours() + ""
+    const minuteNow = dateNow.getUTCMinutes() + ""
+    const dayNow = dateNow.getUTCDay() + ""
+    const monthNow = dateNow.getUTCMonth() + ""
+    const timeNow = monthNow.padStart(2, '0') + '/' + dayNow.padStart(2, '0') + ' ' + hourNow.padStart(2, '0') + ':' + minuteNow.padStart(2, '0')
+    //const timeNow = '11/25 18:00'
+    let prevEvent = null
+    let prevElement = null
+    
 
     events.forEach(event => {
         const li = document.createElement('li')
         li.classList.add("event")
+        
         if (timeNow >= event.date) {
             li.classList.add("active")
+            if (prevEvent !== null && prevEvent.date < event.date) {
+                prevElement.classList.remove("active")
+            }
         }
+        
+        /*if (prevEvent === null && timeNow < event.date) {
+            console.log('lol')
+            document.getElementById('incoming-event').classList.remove('incoming-event')
+        }*/
 
-        li.dataset.date = event.date
+        li.dataset.date = event.dateDisplay
 
         const title = document.createElement('h4')
         title.classList.add("mb-3")
@@ -44,6 +58,9 @@ function displayTimeline(events) {
         li.appendChild(description)
 
         containerEvents.appendChild(li)
+
+        prevEvent = event
+        prevElement = li
     })
 }
 
